2d07902c7f8fb9efb7e583267927453760c236c1
zxl777 <sky@itoytoy.com>
2013-09-28 18:34:33 +0800

    完成了部分界面，待加入sqlite

--- Countdown/CTDAppDelegate.h
+++ Countdown/CTDAppDelegate.h
@@ -9,11 +9,27 @@
 #import <Cocoa/Cocoa.h>
 
 @interface CTDAppDelegate : NSObject <NSApplicationDelegate, NSWindowDelegate>
+{
+    bool ShowOnTop;
+}
 
 @property (assign) IBOutlet NSWindow *window;
+
+
+@property (strong) IBOutlet NSWindow *WorkingTable;
 
 - (IBAction)quit:(id)sender;
 - (IBAction)showHidePanel:(id)sender;
 - (IBAction)showDatePanel:(id)sender;
+
+@property (weak) IBOutlet NSTextField *CurrentGoal;
+
+@property (weak) IBOutlet NSButton *CurrentGoalButton;
+
+@property (unsafe_unretained) IBOutlet NSTextView *APlanView;
+
+@property (unsafe_unretained) IBOutlet NSTextView *GoalView;
+
+@property (unsafe_unretained) IBOutlet NSTextView *BPlanView;
 
 @end
--- Countdown/CTDAppDelegate.m
+++ Countdown/CTDAppDelegate.m
@@ -33,7 +33,8 @@ NSString * const kDoneString = @"Done!";
 - (void)applicationDidFinishLaunching:(NSNotification *)aNotification
 {
     // Insert code here to initialize your application
-    self.countdownDate = [NSDate dateWithNaturalLanguageString:@"Tomorrow 2:00 PM"];
+//    self.countdownDate = [NSDate dateWithNaturalLanguageString:@"Tomorrow 2:00 PM"];
+    self.countdownDate = [NSDate dateWithTimeIntervalSinceNow:25*60];
     [self.datePicker setMinDate:[NSDate date]];
     [self.datePicker setDateValue:self.countdownDate];
     [self.datePicker setTarget:self];
@@ -47,13 +48,70 @@ NSString * const kDoneString = @"Done!";
     [self.statusItem setHighlightMode:YES];
     self.statusItem.menu = self.menu;
     
-    self.timerWindowVisible = NO;
+    self.timerWindowVisible = YES;
+    [self.window setLevel: NSFloatingWindowLevel];
+    [self.window setOpaque:NO];
+    [self.window setAlphaValue:0.6];
+    
+    ShowOnTop = NO;
+    [self TapedWindow:nil];
+//    [self.window setBackgroundColor:[NSColor clearColor]];
+    
+    
     self.calendar = [[NSCalendar alloc] initWithCalendarIdentifier:NSGregorianCalendar];
     
     [self timerFired:self.timer];
     
 }
 
+- (IBAction)TapedSetGoal:(NSMenuItem *)sender
+{
+    [self.WorkingTable setLevel: NSFloatingWindowLevel];
+}
+
+- (void)controlTextDidChange:(NSNotification *)aNotification
+{
+    self.CurrentGoalButton.title =self.CurrentGoal.stringValue;
+}
+
+- (IBAction)TapedWindow:(NSButton *)sender
+{
+    CGRect frame = self.window.frame;
+    ShowOnTop = !ShowOnTop;
+    if (ShowOnTop)
+        frame.origin.y = [NSScreen mainScreen].frame.size.height-frame.size.height-24;
+    else
+        frame.origin.y = 0;
+    
+    
+    [self.window setFrame:frame display:YES];
+}
+
+
+- (IBAction)TapGoalDone:(NSButton *)sender
+{
+    if ([self.CurrentGoal.stringValue length]!=0) //记录完成目标
+    {
+        self.GoalView.string = [NSString stringWithFormat:@"%@\n%@",self.GoalView.string,self.CurrentGoal.stringValue];
+    
+        self.CurrentGoal.stringValue = @"";
+    }
+
+    
+    self.APlanView.string = [self.APlanView.string stringByTrimmingCharactersInSet:
+                                                                    [NSCharacterSet whitespaceAndNewlineCharacterSet]];
+    if ([self.APlanView.string length]==0)
+        return;
+    NSMutableArray *APlanList = [NSMutableArray arrayWithArray:[self.APlanView.string componentsSeparatedByString:@"\n"]];
+    self.CurrentGoal.stringValue = [APlanList lastObject]; // 加子弹
+    
+    [APlanList removeLastObject]; //弹夹下一个子弹
+    self.APlanView.string = [APlanList componentsJoinedByString:@"\n"];
+}
+
+
+
+//一秒执行一次
 - (void)timerFired:(NSTimer *)timer {
     
     NSInteger days, hours, minutes, seconds;
@@ -74,8 +132,11 @@ NSString * const kDoneString = @"Done!";
         return;
     }
     
-    NSString *dayString = (days == 1) ? @"day" : @"days";
-    NSString *labelString = (days == 0) ? [NSString stringWithFormat:@"%li:%02li:%02li", (long)hours, (long)minutes, (long)seconds] : [NSString stringWithFormat:@"%li %@, %li:%02li:%02li", (long)days, dayString, (long)hours, (long)minutes, (long)seconds];
+//    NSString *dayString = (days == 1) ? @"day" : @"days";
+//    NSString *labelString = (days == 0) ? [NSString stringWithFormat:@"%li:%02li:%02li", (long)hours, (long)minutes, (long)seconds] : [NSString stringWithFormat:@"%li %@, %li:%02li:%02li", (long)days, dayString, (long)hours, (long)minutes, (long)seconds];
+
+    NSString *labelString = [NSString stringWithFormat:@"%02li:%02li", (long)minutes, (long)seconds];
+
     
     [self.label setStringValue:labelString];
     [self.statusItem setTitle:labelString];
@@ -101,10 +162,14 @@ NSString * const kDoneString = @"Done!";
     NSMenuItem *item = (NSMenuItem *)sender;
     if (self.timerWindowVisible) {
         [item setTitle:kShowTimerPanelString];
-        [self.window orderOut:nil];
+//        [self.window setLevel:NSStatusWindowLevel];
+//        [self.window orderOut:nil];
+        [[self window] setLevel: NSFloatingWindowLevel];
     } else {
         [item setTitle:kHideTimerPanelString];
-        [self.window makeKeyAndOrderFront:nil];
+//        [self.window makeKeyAndOrderFront:nil];
+//        [self.window setLevel:NSStatusWindowLevel];
+        [[self window] setLevel: NSFloatingWindowLevel];
     }
     self.timerWindowVisible = !self.timerWindowVisible;
 }
